29d041443e416d1e29be0d4c36504819095e45c0
Nahed <nahedben@gmail.com>
2020-06-09 22:24:53 +0300

    this is my second commit, just simple comment

--- src/teshello.java
+++ src/teshello.java
@@ -1,5 +1,7 @@
 public class teshello {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        //this is a java file to be commited into git
         System.out.println("c'est mon test de deposer a Git");
     }
 }
